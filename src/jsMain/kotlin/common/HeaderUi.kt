95cad54aba5d2835e5250fc7d95fe398d5767898
theapache64 <theapache64@gmail.com>
2025-06-08 12:05:07 +0530

    Add tooltip initialization for headers

--- src/jsMain/kotlin/common/HeaderUi.kt
+++ src/jsMain/kotlin/common/HeaderUi.kt
@@ -3,11 +3,16 @@ package common
 import androidx.compose.runtime.Composable
 import org.jetbrains.compose.web.css.cursor
 import org.jetbrains.compose.web.css.marginBottom
+import org.jetbrains.compose.web.css.marginRight
 import org.jetbrains.compose.web.css.marginTop
 import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.css.width
 import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.H1
+import org.jetbrains.compose.web.dom.Img
 import org.jetbrains.compose.web.dom.Text
+
+private const val version = "v25.03.16.1 (16 Mar 2025)"
 
 @Composable
 fun Header(
@@ -17,21 +22,28 @@ fun Header(
         classes("row")
     }) {
         Div(attrs = {
-            classes("col-lg-12")
+            classes("col-lg-12", "text-center")
             style {
                 marginBottom(30.px)
                 marginTop(30.px)
             }
+
         }) {
-            H1(attrs = {
-                classes("text-center")
-                style {
-                    cursor("pointer")
+            H1(
+                attrs = {
+                    attr("data-bs-toggle", "tooltip")
+                    attr("data-bs-placement", "top")
+                    attr("title", version)
+                    style {
+                        cursor("pointer")
+                    }
+                    onClick {
+                        onTitleClicked()
+                    }
                 }
-                onClick {
-                    onTitleClicked()
-                }
-            }) { Text("⚡ diffetto") }
+            ) {
+                Text("⚡ diffetto")
+            }
         }
     }
 }
--- src/jsMain/kotlin/page/input/App.kt
+++ src/jsMain/kotlin/page/input/App.kt
@@ -3,7 +3,16 @@ package page.input
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import common.Header
+import org.jetbrains.compose.web.css.Position
+import org.jetbrains.compose.web.css.bottom
+import org.jetbrains.compose.web.css.marginTop
+import org.jetbrains.compose.web.css.paddingBottom
+import org.jetbrains.compose.web.css.paddingLeft
+import org.jetbrains.compose.web.css.paddingRight
+import org.jetbrains.compose.web.css.position
+import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Text
 import page.result.ResultUi
 import repo.CacheRepoImpl
 
